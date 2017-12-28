f1fe4ed59c79b94fb3216270a4804ecdff51b54a
Marcin <Marcin@Marcin>
2017-12-28 18:32:02 +0100

    grid added

---
+++ page-object-pattern/src/main/java/SeleniumGrid/TestCaseGrid.java
@@ -0,0 +1,37 @@
+package SeleniumGrid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class TestCaseGrid {
+	
+	protected WebDriver driver;
+	private DesiredCapabilities capabilities;
+	private String seleniumHub = "http://xxx.xxx.x.xxx:4444/wd/hub";
+	
+	@Before
+	public void setUp() throws MalformedURLException {
+// 		simple way
+//		DesiredCapabilities capabilities =  DesiredCapabilities.firefox();
+		capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("firefox");
+		capabilities.setVersion("");
+		capabilities.setPlatform(Platform.valueOf("WINDOWS"));
+		driver = new RemoteWebDriver(new URL(seleniumHub), capabilities);
+		driver.manage().window().maximize();
+	
+	}
+	
+	@After
+	public void cleanUp() {
+		driver.quit();
+	}
+	
+}
